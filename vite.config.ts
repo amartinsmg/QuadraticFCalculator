1ffcad118e8d7916e0a0500c11859dbd485d42bd
amartinsmg <andremindurimg@gmail.com>
2026-08-10 20:50:29 -0300

    build: remove Babel and Webpack, add Vite

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import commonjs from 'vite-plugin-commonjs'
+
+export default defineConfig({
+  plugins: [
+    commonjs()
+  ],
+  optimizeDeps: {
+    include: ['algebrite']
+  }
+})
